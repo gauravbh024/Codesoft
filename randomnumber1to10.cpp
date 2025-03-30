86b902241ba959c8f82beea3f6ee922f41037ba6
gauravbh024 <bhargavgaurav427@gmail.com>
2025-03-30 10:51:24 +0530

    here is task 1

---
+++ randomnumber1to10.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void guessTheNumber() {
+    srand(time(0)); 
+    int numberToGuess = rand() % 10 + 1; 
+    int guess = 0;
+    
+    cout << "Welcome To The Number Guess Game! I Am Challenging You To Guess My Number Correctly Between 1 And 100." << endl;
+    cout << "I Chalenge You Tu Guess It Right I Know You Can Not Do This LOL" << endl;
+    
+    while (guess != numberToGuess) {
+        cout << "Tell Me What You Guessed: ";
+        cin >> guess;
+        
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Why Entering Wrong Number ? Try Again Bro" << endl;
+            continue;
+        }
+        
+        if (guess < numberToGuess) {
+            cout << "Nahh You Are Guessing Too Low" << endl;
+        } else if (guess > numberToGuess) {
+            cout << "Nahh You Are Guessing To High" << endl;
+        } else {
+            cout << "Congratulations! I Dont Belive That You Get It . You Guessed It Correct" << endl;
+            cout<<"You Are The Best I Admit My Defeat , Thanks For Playing "<<endl;
+        }
+    }
+}
+
+int main() {
+    guessTheNumber();
+    return 0;
+}
